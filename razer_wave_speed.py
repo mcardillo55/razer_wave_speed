7b9e63627aa78159de66025dd563c78a0ef83e7c
Mike Cardillo <mcardillo55@gmail.com>
2017-05-11 02:50:35 -0700

    Use pickle data if not pcap file. Switched tabs to spaces.

--- razer_wave_speed.py
+++ razer_wave_speed.py
@@ -1,43 +1,48 @@
 # Author: Mike Cardillo, 2017
 #  Email: mcardillo55@gmail.com
 #   Desc: Control speed of the Razer 'wave' effect by sending custom values to
-#		  the razerkbd driver and varying the sleep() time.
+#         the razerkbd driver and varying the sleep() time.
 
 import pyshark
 import time
 import os, sys
+import pickle
 
 RAZERKBD_SYSFS_PATH = '/sys/bus/hid/drivers/razerkbd/0003:1532:0221.0013/'
 PCAP_FILE = '/home/mike/razer_packets.pcapng'
 
+if os.path.isfile('razr_wave_data.dat'):
+    print("using pickle")
+    with open('razr_wave_data.dat', 'rb') as wave_data:
+        slow_wave_pattern = pickle.load(wave_data)
+else:
+    print('analyzing')
+    cap = pyshark.FileCapture(PCAP_FILE)
 
-cap = pyshark.FileCapture(PCAP_FILE)
+    slow_wave_pattern = []
 
-slow_wave_pattern = []
-
-for idx, cur_cap in enumerate(cap):
-	if idx == 966:
-		# This is when the pattern repeats.
-		break
-	cap_data = cur_cap.data.usb_data_fragment
-	if cap_data.binary_value[8] == 255:
-		# First 8 bytes are control bytes and not included
-		# Next are row index, row start and row stop
-		# Then 22 RGB values
-		slow_wave_pattern.append(cap_data.binary_value[9:78])
-
-print("done analyzing")
-print(len(slow_wave_pattern))
+    for idx, cur_cap in enumerate(cap):
+        if idx == 966:
+            # This is when the pattern repeats.
+            break
+        cap_data = cur_cap.data.usb_data_fragment
+        if cap_data.binary_value[8] == 255:
+            # First 8 bytes are control bytes and not included
+            # Next are row index, row start and row stop
+            # Then 22 RGB values
+            slow_wave_pattern.append(cap_data.binary_value[9:78])
+    with open('razr_wave_data.dat', 'wb') as wave_data:
+        pickle.dump(slow_wave_pattern, wave_data)
 
 while True:
-	for pattern in slow_wave_pattern:
-		if pattern[0] == 0:
-			with open(os.path.join(RAZERKBD_SYSFS_PATH, 'matrix_effect_custom'), 'w') as enable_custom_effect:
-				enable_custom_effect.write('1')
+    for pattern in slow_wave_pattern:
+        if pattern[0] == 0:
+            with open(os.path.join(RAZERKBD_SYSFS_PATH, 'matrix_effect_custom'), 'w') as enable_custom_effect:
+                enable_custom_effect.write('1')
 
-		with open(os.path.join(RAZERKBD_SYSFS_PATH, 'matrix_custom_frame'), 'wb') as custom_frame:
-			custom_frame.write(pattern)
+        with open(os.path.join(RAZERKBD_SYSFS_PATH, 'matrix_custom_frame'), 'wb') as custom_frame:
+            custom_frame.write(pattern)
 
-		sys.stdout.flush()
+        sys.stdout.flush()
 
-		time.sleep(0.0010)
+        time.sleep(0.0010)
