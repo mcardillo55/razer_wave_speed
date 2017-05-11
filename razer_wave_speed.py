2155970f4eb033ce521032dc5a37062365c5a8b2
Mike Cardillo <mcardillo55@gmail.com>
2017-05-11 05:13:07 -0700

    Added command line arguments

--- razer_wave_speed.py
+++ razer_wave_speed.py
@@ -3,22 +3,41 @@
 #   Desc: Control speed of the Razer 'wave' effect by sending custom values to
 #         the razerkbd driver and varying the sleep() time.
 
-
 import time
 import os, sys
 import pickle
+import argparse
 
 RAZERKBD_SYSFS_PATH = '/sys/bus/hid/drivers/razerkbd/0003:1532:0221.0013/'
 PCAP_FILE = '/home/mike/razer_packets.pcapng'
 
+def check_bounds(string):
+    try:
+        value = int(string)
+    except:
+        raise argparse.ArgumentTypeError("speed must be an integer")
+    if value < 0 or value > 100:
+        raise argparse.ArgumentTypeError("speed must be between 0-100")
+    return value
+
+parser = argparse.ArgumentParser(description='Control the speed of the wave effect on Razer keyboards')
+parser.add_argument('speed', type=check_bounds, help='Speed from 0-100')
+parser.add_argument('-v', action='store_true', help='Add debug information')
+
+args = parser.parse_args()
+
+wave_speed = (100 - args.speed) * 0.0005
+
 if os.path.isfile('razr_wave_data.dat'):
-    print("using pickle")
+    if args.v:
+        print("Using pickle.")
     with open('razr_wave_data.dat', 'rb') as wave_data:
         slow_wave_pattern = pickle.load(wave_data)
 else:
     import pyshark
-    
-    print('analyzing')
+
+    if args.v:
+        print('Analyzing...')
     cap = pyshark.FileCapture(PCAP_FILE)
 
     slow_wave_pattern = []
@@ -50,4 +69,4 @@ while True:
                 enable_custom_effect.write('1')
             sys.stdout.flush()
 
-        time.sleep(0.005)
+        time.sleep(wave_speed)
